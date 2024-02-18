d0128cad9248dfafa2d30f3bd2af5b53eb303be8
Kartavaya0508 <kartavayajn05@gmail.com>
2024-02-18 20:05:23 +0530

    Completed all tpes of opertaors

--- 06_Expresssions_and_Conditionals.js
+++ 06_Expresssions_and_Conditionals.js
@@ -22,6 +22,44 @@ console.log("a = ", a)
 a = 8
 console.log(a)
 
-let c = 5
+let assignment = 5
 assignment += 5 // same as assignment = assignment + 5
-assignment += 5 // same as assignment = assignment + 5
+console.log(assignment)
+
+assignment -= 5
+console.log(assignment)
+
+assignment *= 5
+console.log(assignment)
+
+assignment /= 5
+console.log(assignment)
+
+assignment **= 5
+console.log(assignment)
+
+assignment %= 5
+console.log(assignment)
+
+// Comparison operators
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is", comp1 == comp2)
+console.log("comp1 != comp2 is", comp1 != comp2)
+console.log("comp1 === comp2 is", comp1 === comp2)
+console.log("comp1 !== comp2 is", comp1 !== comp2)
+
+let d = 6;
+let e = "6"
+console.log("e == d is", e == d)
+console.log("e != d is", e != d)
+console.log("e === d", e === d)
+console.log("e !== d is", e !== d)
+
+// Logical Operators
+
+let x = 5;
+let y = 6;
+console.log(x<y && x == 5)
+console.log(x>y || x == 5)
+console.log(!true)
