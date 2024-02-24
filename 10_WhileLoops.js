bc64fa6481490b766f3eb60bab51eefabaf3cf7f
Kartavaya0508 <kartavayajn05@gmail.com>
2024-02-24 14:00:41 +0530

    Completed While Loop

---
+++ 10_WhileLoops.js
@@ -0,0 +1,21 @@
+// While Loop
+
+let n = prompt("Enter the value of n")
+n =Number.parseInt(n)
+
+let i=0;
+while(i<n){
+    console.log(i)
+    i++;
+}
+
+// DO WHILE Loop
+
+let m = prompt("Enter the value of n")
+m = Number.parseInt(m)
+
+let k = 10
+do{
+    console.log(k)
+    k++;
+} while(k<m)
