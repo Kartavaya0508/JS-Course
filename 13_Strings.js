d873facd2ad70404be7cd07c63c8c72ddad8129d
Kartavaya0508 <kartavayajn05@gmail.com>
2024-03-02 20:19:44 +0530

    Started Strings and did it's syntax

---
+++ 13_Strings.js
@@ -0,0 +1,39 @@
+// Double qouted string
+let NAME = "Kartavaya"
+console.log(NAME)
+console.log(NAME.length)
+
+// Single quoted string
+let gg = 'Kussh'
+console.log(gg)
+
+// Template Literals
+let boy1 = "Kartavaya"
+let girl1 = "Janvi"
+console.log(`${boy1} is a friend of ${girl1}`)
+
+// or (String Interpolation)
+let sentence = `${boy1} is a friend of ${girl1}`
+console.log(sentence)
+
+// Escape Sequence Characters
+// Single Quote Escape Sequence
+let fname = 'D\'Jan\'Milio'
+console.log(fname)
+
+// Double Quote Escape Sequence
+let g = "Bana\"na"
+console.log(g)
+
+// New line
+let n = 'I don\'t \n know'
+console.log(n)
+
+//Tab
+let p = 'Lets have some \tsnacks'
+console.log(p)
+
+// Carriage Return
+let s = 'What is your \rname'
+console.log(s)
+
